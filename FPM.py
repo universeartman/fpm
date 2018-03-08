4250ec9bf5f30e3f937d4dc4d26960b9d379e158
universeartman <36418607+universeartman@users.noreply.github.com>
2018-03-08 13:21:29 -0600

    Add files via upload

---
+++ FPM.py
@@ -0,0 +1,16 @@
+""" FPM calculator inputs MPH, Distance to target and delta altitude and
+outputs FPM rquirements """
+
+def fpm():
+    ''' Input the required data and set as usable variables'''
+    ''' Input MPH and set as mph'''
+    
+    '''Input distnce in miles and set as dist'''
+    
+    '''Input delta altitude in feet and set as alt_d'''
+    
+    """Make the conversions required to do the math"""
+    
+    '''Do the math'''
+    
+    """Print the required FPM requirement"""
